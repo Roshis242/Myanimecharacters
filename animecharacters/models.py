503b0aa7257d3940f6795e1c6216da23e2726d93
Roshis <rockyrd.12458@gmail.com>
2021-06-19 21:02:39 +0545

    Removed id field form models

--- animecharacters/models.py
+++ animecharacters/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Animecharacter(models.Model):
-    id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     alias_name = models.CharField(max_length=200)
